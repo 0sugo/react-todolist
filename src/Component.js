483103c08e3f80b6978a8115ad34afc6f0610179
0sugo <josugo26@gmail.com>
2023-05-02 23:58:14 +0300

    Add component to house individual items

---
+++ src/Component.js
@@ -0,0 +1,13 @@
+import  ItemList  from './ItemList';
+
+export default function Component({handleCheck,handleDelete,items}) {
+  
+  return (
+    <div>
+      {(items.length===0) ?"All Items done!!!!":"" }
+     <ItemList items={items} handleCheck={handleCheck} handleDelete ={handleDelete} />
+       
+    </div>
+  )
+}
+ 
